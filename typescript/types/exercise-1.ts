78b5bbbe7f728bdcb4a597cad73472597411072e
Ray-M05 <rachelmojena@gmail.com>
2025-03-06 22:52:26 -0500

    TS exercises done

--- typescript/types/exercise-1.ts
+++ typescript/types/exercise-1.ts
@@ -1,33 +1,47 @@
 // Exercise 1: Define a variable with a string type and assign it a value
-let myString;
+let myString: string = "Hello World";
 
 // Exercise 2: Define a variable with a number type and assign it a value
-let myNumber;
+let myNumber: number = 5;
 
 // Exercise 3: Define a variable with a boolean type and assign it a value
-let myBoolean;
+let myBoolean: boolean = true;
 
 // Exercise 4: Define an array of numbers
-let myNumberArray;
+let myNumberArray: number[] = [1,2];
 
 // Exercise 5: Define an array of strings
-let myStringArray;
+let myStringArray: string[] = ["1","2"];
 
 // Exercise 6: Define a tuple with a string and a number
-let myTuple;
+let myTuple: [number, string] = [1,'a'] ;
 
 // Exercise 7: Create an enum with three values and assign it to a variable
-enum MyEnum {}
+enum MyEnum {
+  High,
+  Medium,
+  Low
+}
+let level: MyEnum = MyEnum.High
 
 // Exercise 8: Define an object interface with specific properties and assign it to a variable
-interface MyObject {}
+interface MyObject {
+  level: string;
+  ocupation: string;
+  years: number
+}
+let person: MyObject = {
+  level: "high",
+  ocupation: "fe",
+  years: 19
+}
 
 // Exercise 9: Define a function that takes two numbers and returns their sum, take a look at the function signature
-function addNumbers(a, b) {
-  // Implement the function
+function addNumbers(a: number, b: number): number {
+  return a+b
 }
 
 // Exercise 10: Define a function that takes a string and returns its length, take a look at the function signature
-function getStringLength(str) {
-  // Implement the function
+function getStringLength(str: string): number {
+  return str.length
 }
--- typescript/types/exercise-2.ts
+++ typescript/types/exercise-2.ts
@@ -2,17 +2,19 @@
 export type MyType = {
   property1: string;
   property2: number;
+  property3: boolean;
 };
 
 // Create a function that takes an argument of type MyType and returns a string
 export function describeMyType(obj: MyType): string {
-  return `Property1 is ${obj.property1} and Property2 is ${obj.property2}`;
+  return `Property1 is ${obj.property1} and Property2 is ${obj.property2} and Property3 is ${obj.property3}`;
 }
 
 // Create a variable of type MyType and assign values to it
 const myObject: MyType = {
   property1: "Hello",
   property2: 42,
+  property3: true,
 };
 
 // Call the function with the variable and log the result
@@ -31,12 +33,26 @@ console.log(describeMyType(myObject));
     4. Create a variable of type IntersectionType and assign values to it.
     5. Call the function with the variable and log the result.
 */
+type TypeA = {
+  a:string
+}
 
-export type IntersectionType = {};
+type TypeB = {
+  b: number
+}
+
+export type IntersectionType = TypeA & TypeB;
 
 export function describeIntersectionType(obj: IntersectionType) {
   return `Property a is ${obj.a} and Property b is ${obj.b}`;
 }
+
+const ex2 : IntersectionType = {
+  a: "a",
+  b: 1,
+}
+
+console.log(describeIntersectionType(ex2))
 
 /* Exercise 3:
   1. Define an interface called MyInterface with three properties: prop1 (string), prop2 (number), and prop3 (boolean).
@@ -44,9 +60,20 @@ export function describeIntersectionType(obj: IntersectionType) {
   3. Create a variable(myInterfaceObject) of type MyInterface and assign values World for prop1, 100 to prop2 and true to prop3.
   4. Call the function with the variable and log the result.
 */
+interface MyInterface {
+  prop1: string,
+  prop2: number,
+  prop3: boolean
+}
 
 export function describeMyInterface(obj: MyInterface): string {
   return `Prop1 is ${obj.prop1}, Prop2 is ${obj.prop2}, and Prop3 is ${obj.prop3}`;
+}
+
+const myInterfaceObject: MyInterface = {
+  prop1: "World",
+  prop2: 100,
+  prop3: true
 }
 
 console.log(describeMyInterface(myInterfaceObject));
@@ -58,12 +85,16 @@ console.log(describeMyInterface(myInterfaceObject));
   4. Create a variable of type ExcludedType and assign values to  World for prop1 and true to prop3.
   5. Call the function with the variable and log the result.
 */
+type ExcludedType = Omit<FullType,"prop2">
 
 export function describeExcludedType(obj: ExcludedType): string {
   return `Prop1 is ${obj.prop1} and Prop3 is ${obj.prop3}`;
 }
 
-const excludedObject;
+const excludedObject: ExcludedType = {
+  prop1: 'World',
+  prop3:true,
+};
 
 console.log(describeExcludedType(excludedObject));
 
@@ -74,16 +105,21 @@ console.log(describeExcludedType(excludedObject));
   4. Create a variable of type OmittedType and assign values to prop1 and prop3.
   5. Call the function with the variable and log the result.
 */
-
 type FullType = {
+  prop1: string,
+  prop2: number,
+  prop3: boolean
 };
 
-type OmittedType = {};
+type OmittedType = Omit<FullType, "prop2">;
 
 export function describeOmittedType(obj: OmittedType): string {
   return `Prop1 is ${obj.prop1} and Prop3 is ${obj.prop3}`;
 }
 
-const omittedObject: OmittedType
+const omittedObject: OmittedType ={
+  prop1: 'World',
+  prop3:true,
+}
 
 console.log(describeOmittedType(omittedObject));
